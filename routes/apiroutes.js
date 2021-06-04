278d53ffb47d9d0cecc93ba37cb0be82e2af17b5
Alexandra Murawski <almurawski15@gmail.com>
2021-06-04 09:54:11 -0400

    created routes

---
+++ routes/apiroutes.js
@@ -0,0 +1,29 @@
+const db = require("../models");
+
+module.exports = function(app) {
+
+    app.get("/api/workouts", (req, res) => {
+        db.Fitness.find({}).then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        catch(err => {
+            res.status(400).json(err);
+        });
+    })
+
+    app.get("/api/workouts/range", ({}, res) => {
+        db.Fitness.find({}).then((dbWorkout) => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.status(400).json(err);
+        });
+    });
+
+    app.post("/api/workouts/", (req, res) => {
+        db.Fitness.create(req.body).then((dbWorkout) => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.status(400).json(err);
+        });
+    });
+}
--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness-tracker", {
     userNewUrlParser: true,
     useFindAndModify: false
 });
